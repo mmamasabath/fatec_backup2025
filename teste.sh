1709f986b233a3767ea0d57a78a9ddd58e6bbbdb
mmamasabath <marciareginaresperanto@gmail.com>
2026-03-02 23:03:11 -0300

    backup 2025 part 4
    revisao de codigos

---
+++ teste.sh
@@ -0,0 +1,15 @@
+                                   
+echo '                    / V\   '  
+printf '                  / `  /      +----------------+ \n'    
+printf '                 <<   |       |  RaPoSa TeCh   |\n'    
+printf '                  /    |      +----------------+\n'    
+printf '                /      |      \n'    
+printf '              /        |      +------------------------+\n'    
+printf '             /    \  \ /      | Tecnologia em software |\n'     
+printf '            (      ) | |      |             2024 - 2026|\n'     
+printf '    ________|   _/_  | |      +------------------------+\n'     
+printf '  <__________\______)\__) \n'     
+
+printf "\n \n"
+
+printf "ola mundo\n"
